807005f773956be9ac5100d837e6ede9b3d7d147
Willy Scheibel <willy.scheibel@hpi.de>
2021-03-07 19:03:23 +0100

    Remove temporary file (closes #1)

--- service/v1/__init__.py
+++ service/v1/__init__.py
@@ -9,7 +9,7 @@ import json
 from pydantic import BaseModel
 
 from fastapi import APIRouter, Response, File, UploadFile, Form
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, BackgroundTasks
 from fastapi.responses import FileResponse
 
 
@@ -19,6 +19,16 @@ router = APIRouter()
 GLKERNEL_DIRECTORY = os.environ.get('GLKERNEL_DIRECTORY', '')
 RESULT_DIR = os.environ.get('RESULT_DIR', '/data/results')
 WORKING_DIRECTORY = '/opt/dependencies/glkernel/'
+
+
+def remove_temporary_files(file_ids, file_names):
+    for f, f_name in zip(file_ids, file_names):
+        print("Remove", f_name, flush=True)
+        try:
+            os.close(f)
+            os.remove(f_name)
+        except:
+            pass
 
 
 # ROOT
@@ -36,7 +46,7 @@ async def head_root(response: Response):
 # IMAGES
 
 @router.post("/image_from_script", tags=["image"])
-async def get_image_from_script(script: str = Form(...)):
+async def get_image_from_script(background_tasks: BackgroundTasks, script: str = Form(...)):
     script_f, script_f_name = tempfile.mkstemp(suffix=".js", text=True)
     with open(script_f, 'w+t') as f:
         f.write(script)
@@ -56,12 +66,14 @@ async def get_image_from_script(script: str = Form(...)):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=p.stderr.decode("utf-8"),
         )
+    
+    background_tasks.add_task(remove_temporary_files, [script_f, image_f], [script_f_name, image_f_name])
 
     return FileResponse(image_f_name, filename="kernel.png", media_type='image/png')
 
 
 @router.post("/image_from_script_file", tags=["image"])
-async def get_image_from_script_file(script_file: UploadFile = File(...)):
+async def get_image_from_script_file(background_tasks: BackgroundTasks, script_file: UploadFile = File(...)):
     script_f, script_f_name = tempfile.mkstemp(suffix=".js", text=True)
     with open(script_f, 'w+b') as f:
         shutil.copyfileobj(script_file.file, f)
@@ -82,11 +94,13 @@ async def get_image_from_script_file(script_file: UploadFile = File(...)):
             detail=p.stderr.decode("utf-8"),
         )
 
+    background_tasks.add_task(remove_temporary_files, [script_f, image_f], [script_f_name, image_f_name])
+
     return FileResponse(image_f_name, filename="kernel.png", media_type='image/png')
 
 
 @router.post("/image_from_declaration", tags=["image"])
-async def get_image_from_declaration(declaration: str = Form(...)):
+async def get_image_from_declaration(background_tasks: BackgroundTasks, declaration: str = Form(...)):
     declaration_f, declaration_f_name = tempfile.mkstemp(suffix=".json", text=True)
     with open(declaration_f, 'w+t') as f:
         f.write(declaration)
@@ -123,11 +137,13 @@ async def get_image_from_declaration(declaration: str = Form(...)):
             detail=p.stderr.decode("utf-8"),
         )
 
+    background_tasks.add_task(remove_temporary_files, [declaration_f, array_f, image_f], [declaration_f_name, array_f_name, image_f_names])
+
     return FileResponse(image_f_name, filename="kernel.png", media_type='image/png')
 
 
 @router.post("/image_from_declaration_file", tags=["image"])
-async def get_image_from_declaration_file(declaration_file: UploadFile = File(...)):
+async def get_image_from_declaration_file(background_tasks: BackgroundTasks, declaration_file: UploadFile = File(...)):
     declaration_f, declaration_f_name = tempfile.mkstemp(suffix=".json", text=True)
     with open(declaration_f, 'w+b') as f:
         shutil.copyfileobj(declaration_file.file, f)
@@ -164,11 +180,13 @@ async def get_image_from_declaration_file(declaration_file: UploadFile = File(..
             detail=p.stderr.decode("utf-8"),
         )
 
+    background_tasks.add_task(remove_temporary_files, [declaration_f, array_f, image_f], [declaration_f_name, array_f_name, image_f_name])
+
     return FileResponse(image_f_name, filename="kernel.png", media_type='image/png')
 
 
 @router.post("/image_from_array", tags=["image"])
-async def get_image_from_array(array: str = Form(...)):
+async def get_image_from_array(background_tasks: BackgroundTasks, array: str = Form(...)):
     array_f, array_f_name = tempfile.mkstemp(suffix=".json", text=True)
     with open(array_f, 'w+t') as f:
         f.write(array)
@@ -189,11 +207,13 @@ async def get_image_from_array(array: str = Form(...)):
             detail=p.stderr.decode("utf-8"),
         )
 
+    background_tasks.add_task(remove_temporary_files, [array_f, image_f], [array_f_name, image_f_name])
+
     return FileResponse(image_f_name, filename="kernel.png", media_type='image/png')
 
 
 @router.post("/image_from_array_file", tags=["image"])
-async def get_image_from_array_file(array_file: UploadFile = File(...)):
+async def get_image_from_array_file(background_tasks: BackgroundTasks, array_file: UploadFile = File(...)):
     array_f, array_f_name = tempfile.mkstemp(suffix=".json", text=True)
     with open(array_f, 'w+b') as f:
         shutil.copyfileobj(array_file.file, f)
@@ -214,13 +234,15 @@ async def get_image_from_array_file(array_file: UploadFile = File(...)):
             detail=p.stderr.decode("utf-8"),
         )
 
+    background_tasks.add_task(remove_temporary_files, [array_f, image_f], [array_f_name, image_f_name])
+
     return FileResponse(image_f_name, filename="kernel.png", media_type='image/png')
 
 
 # ARRAY
 
 @router.post("/array_from_script", tags=["array"])
-async def get_array_from_script(script: str = Form(...)):
+async def get_array_from_script(background_tasks: BackgroundTasks, script: str = Form(...)):
     script_f, script_f_name = tempfile.mkstemp(suffix=".js", text=True)
     with open(script_f, 'w+t') as f:
         f.write(script)
@@ -241,11 +263,13 @@ async def get_array_from_script(script: str = Form(...)):
             detail=p.stderr.decode("utf-8"),
         )
 
+    background_tasks.add_task(remove_temporary_files, [script_f, array_f], [script_f_name, array_f_name])
+
     return FileResponse(array_f_name, filename="kernel.json", media_type='application/json')
 
 
 @router.post("/array_from_script_file", tags=["array"])
-async def get_array_from_script_file(script_file: UploadFile = File(...)):
+async def get_array_from_script_file(background_tasks: BackgroundTasks, script_file: UploadFile = File(...)):
     script_f, script_f_name = tempfile.mkstemp(suffix=".js", text=True)
     with open(script_f, 'w+b') as f:
         shutil.copyfileobj(script_file.file, f)
@@ -266,11 +290,13 @@ async def get_array_from_script_file(script_file: UploadFile = File(...)):
             detail=p.stderr.decode("utf-8"),
         )
 
+    background_tasks.add_task(remove_temporary_files, [script_f, array_f], [script_f_name, array_f_name])
+
     return FileResponse(array_f_name, filename="kernel.json", media_type='application/json')
 
 
 @router.post("/array_from_declaration", tags=["array"])
-async def get_array_from_declaration(declaration: str = Form(...)):
+async def get_array_from_declaration(background_tasks: BackgroundTasks, declaration: str = Form(...)):
     declaration_f, declaration_f_name = tempfile.mkstemp(suffix=".json", text=True)
     with open(declaration_f, 'w+t') as f:
         f.write(declaration)
@@ -291,11 +317,13 @@ async def get_array_from_declaration(declaration: str = Form(...)):
             detail=p.stderr.decode("utf-8"),
         )
 
+    background_tasks.add_task(remove_temporary_files, [declaration_f, array_f], [declaration_f_name, array_f_name])
+
     return FileResponse(array_f_name, filename="kernel.json", media_type='application/json')
 
 
 @router.post("/array_from_declaration_file", tags=["array"])
-async def get_array_from_declaration_file(declaration_file: UploadFile = File(...)):
+async def get_array_from_declaration_file(background_tasks: BackgroundTasks, declaration_file: UploadFile = File(...)):
     declaration_f, declaration_f_name = tempfile.mkstemp(suffix=".json", text=True)
     with open(declaration_f, 'w+b') as f:
         shutil.copyfileobj(declaration_file.file, f)
@@ -315,5 +343,7 @@ async def get_array_from_declaration_file(declaration_file: UploadFile = File(..
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=p.stderr.decode("utf-8"),
         )
+
+    background_tasks.add_task(remove_temporary_files, [declaration_f, array_f], [declaration_f_name, array_f_name])
 
     return FileResponse(array_f_name, filename="kernel.json", media_type='application/json')
